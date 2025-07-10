e16c509747f3c2c7e05c8ea56f2d01f19b613d8f
Janus Helkjær <janus.helkjaer@gmail.com>
2025-07-10 21:58:53 +0200

    filament initial resources

---
+++ app/Filament/Resources/ArtistResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ArtistResource\Pages;
+use App\Filament\Resources\ArtistResource\RelationManagers;
+use Domain\Artist\Models\Artist;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ArtistResource extends Resource
+{
+    protected static ?string $model = Artist::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('first_name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('last_name')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('alias'),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('first_name'),
+                Tables\Columns\TextColumn::make('last_name'),
+                Tables\Columns\TextColumn::make('alias'),
+                Tables\Columns\TextColumn::make('bio'),
+                Tables\Columns\TextColumn::make('website'),
+                Tables\Columns\TextColumn::make('profile_image_path'),
+                Tables\Columns\TextColumn::make('created_at'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListArtists::route('/'),
+            'create' => Pages\CreateArtist::route('/create'),
+            'edit' => Pages\EditArtist::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/ArtworkResource.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ArtworkResource\Pages;
+use App\Filament\Resources\ArtworkResource\RelationManagers;
+use Domain\Artwork\Models\Artwork;
+use Domain\Artist\Models\Artist;
+use Domain\Gallery\Models\Gallery;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ArtworkResource extends Resource
+{
+    protected static ?string $model = Artwork::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title'),
+                Forms\Components\TextInput::make('description'),
+                Forms\Components\TextInput::make('year'),
+                Forms\Components\TextInput::make('medium'),
+                Forms\Components\TextInput::make('height')->numeric(),
+                Forms\Components\TextInput::make('width')->numeric(),
+                Forms\Components\TextInput::make('depth')->numeric(),
+                Forms\Components\TextInput::make('weight')->numeric(),
+                Forms\Components\TextInput::make('price')->numeric(),
+                Forms\Components\Select::make('status')
+                    ->options([
+                        'available' => 'Available',
+                        'sold' => 'Sold',
+                        'reserved' => 'Reserved',
+                        'not_for_sale' => 'Not for sale',
+                    ])->default('available'),
+                Forms\Components\Select::make('artist_id')
+                    ->relationship('artist', 'first_name'),
+                Forms\Components\Select::make('gallery_id')
+                    ->relationship('gallery', 'name'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\TextColumn::make('description'),
+                Tables\Columns\TextColumn::make('year'),
+                Tables\Columns\TextColumn::make('medium'),
+                Tables\Columns\TextColumn::make('dimensions'),
+                Tables\Columns\TextColumn::make('price'),
+                Tables\Columns\TextColumn::make('status'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListArtworks::route('/'),
+            'create' => Pages\CreateArtwork::route('/create'),
+            'edit' => Pages\EditArtwork::route('/{record}/edit'),
+        ];
+    }
+}
